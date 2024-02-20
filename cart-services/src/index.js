1f618014d06d43106e00d1a987d2ba1a96d31014
kayane <kayane.fernandez.pro1@gmail.com>
2024-02-20 16:30:28 +0100

    add connexion database dans index.js

--- cart-services/src/index.js
+++ cart-services/src/index.js
@@ -4,15 +4,20 @@ const app = express();
 const mongoose = require("mongoose");
 
 // Route du crud simple du panier
-const cartRoutes = require("./routes/cartRoutes");
+// const cartRoutes = require("./routes/cartRoutes");
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
 
-app.get('/', (req, res) => {
-    res.send('cart services (panier) en fonctionnement');
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connecté à mongo"))
+  .catch((err) => console.log(err));
+
+// app.use("/cart", cartRoutes);
+
+
 
 app.listen(PORT, ()=> {
     console.log(`Serveur en cours d'execution sur le port ${PORT}`);
