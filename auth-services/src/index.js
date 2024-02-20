58f04fc576a0766cef8e8c2fa2e122e1d43193e7
Simon <lascaudsim@gmail.com>
2024-02-20 14:53:46 +0100

    auth-service

--- auth-services/src/index.js
+++ auth-services/src/index.js
@@ -2,14 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
-
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
 
-let users = [];
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
@@ -23,15 +33,16 @@ app.get("/", (req, res) => {
 app.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
+    console.log(req.body);
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const userExists = users.find((user) => user.username === username);
+    const userExists = await User.findOne({ username: username });
     if (userExists) {
       return res.status(400).json({ message: "Cet utilisateur existe déjà" });
     }
-    const user = { username, password: hashedPassword };
-    users.push(user);
-    res.status(201).send("Utilisateur enregistré avec succès: $user");
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).send(`Utilisateur enregistré avec succès: $user`);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -40,10 +51,12 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = users.find((user) => user.username === username);
+    const user = await User.findOne({ username: username });
     if (user && (await bcrypt.compare(password, user.password))) {
-      const accessToken = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "2h" });
+      const accessToken = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: "2h" });
       res.status(200).json({ accessToken });
+    } else {
+      res.status(401).send("Nom d'utilisateur ou mot de passe incorrect");
     }
   } catch (err) {
     return res.status(500).json({ message: err.message });
